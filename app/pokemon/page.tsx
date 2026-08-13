cdd81e165313de4d40d2b7ff9d86298ead778188
lacomarcaventas-del <lacomarcaventas@gmail.com>
2026-08-12 22:37:55 -0600

    Crea catálogo Pokémon

---
+++ app/pokemon/page.tsx
@@ -0,0 +1,2 @@
+import Catalog from "../../components/Catalog";
+export default function PokemonPage(){return <><header className="top"><a href="/">← La Comarca</a><b>Pokémon TCG</b><a href="/admin">Admin</a></header><main className="wrap"><Catalog/></main></>}
