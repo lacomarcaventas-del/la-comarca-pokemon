eab861242981e5619dd2ab38b611b9e1047c7263
lacomarcaventas-del <lacomarcaventas@gmail.com>
2026-08-14 20:42:41 -0600

    Mejorar tamaño del feed de Facebook y visibilidad de publicaciones/fotos

--- components/FacebookFeed.tsx
+++ components/FacebookFeed.tsx
@@ -7,5 +7,17 @@ export default function FacebookFeed(){
   if(!existing){const s=document.createElement("script");s.id="facebook-jssdk";s.async=true;s.defer=true;s.crossOrigin="anonymous";s.src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v25.0";document.body.appendChild(s)}
   else if((window as any).FB) (window as any).FB.XFBML.parse();
  },[]);
- return <section className="panel" style={{marginTop:24}}><div className="categoryHeader"><div><h2>📢 La Comarca en Facebook</h2><p className="muted">Publicaciones recientes de nuestra página oficial.</p></div><a className="btn2" href="https://www.facebook.com/ComarcaTCG" target="_blank" rel="noreferrer">Ver Facebook</a></div><div style={{display:"flex",justifyContent:"center",marginTop:16,overflowX:"auto"}}><div className="fb-page" data-href="https://www.facebook.com/ComarcaTCG" data-tabs="timeline" data-width="700" data-height="700" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/ComarcaTCG" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ComarcaTCG">La Comarca</a></blockquote></div></div></section>;
+ return <section className="panel" style={{marginTop:24}}>
+  <div className="categoryHeader">
+   <div><h2>📢 La Comarca en Facebook</h2><p className="muted">Publicaciones recientes de nuestra página oficial.</p></div>
+   <a className="btn2" href="https://www.facebook.com/ComarcaTCG" target="_blank" rel="noreferrer">Ver Facebook</a>
+  </div>
+  <div style={{display:"flex",justifyContent:"center",width:"100%",marginTop:16}}>
+   <div style={{width:"100%",maxWidth:900,minHeight:900,display:"flex",justifyContent:"center",overflow:"hidden"}}>
+    <div className="fb-page" data-href="https://www.facebook.com/ComarcaTCG" data-tabs="timeline" data-width="900" data-height="1000" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" style={{width:"100%"}}>
+     <blockquote cite="https://www.facebook.com/ComarcaTCG" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ComarcaTCG">La Comarca</a></blockquote>
+    </div>
+   </div>
+  </div>
+ </section>;
 }
